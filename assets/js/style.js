eff20dcfe3a58c49eb12623e9ee9a089b1ba4a88
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2023-07-18 22:44:56 +0600

    Add

--- assets/js/style.js
+++ assets/js/style.js
@@ -15,14 +15,3 @@ menuLists.forEach(menuList => {
     subMenu.classList.toggle('open');
   });
 });
-
-
-// Toggle Password
-const togglePassword = document.querySelector('#togglePassword');
-const passwordInput = document.querySelector('#InputPassword');
-
-togglePassword.addEventListener('click', function () {
-  const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-  passwordInput.setAttribute('type', type);
-  togglePassword.innerHTML = type === 'password' ? '<i class="fas fa-eye-slash icons"></i>' : '<i class="fas fa-eye icons"></i>';
-});
